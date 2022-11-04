495a82a9d262d0c6125830d2a3689a0cc07c654c
Rafael <rafalevel100@hotmail.com>
2022-11-04 14:00:33 -0300

    adicionando arquivos da pasta raiz

---
+++ CRUD_Livros/LivrosWEBAPI/wwwroot/controller/RepositorioDeLivros.js
@@ -0,0 +1,78 @@
+sap.ui.define([
+    "sap/ui/base/ManagedObject",
+], function (
+    ManagedObject,
+) {
+    "use strict";
+    const urlAPI = "https://localhost:7012/livros/"
+    const caminhoDoRepositorio = "sap.ui.demo.walkthrough.controller.RepositorioDeLivros"
+    return ManagedObject.extend(caminhoDoRepositorio, {
+        //ObterTodosOsLivros
+        ObterTodosOsLivros: function () {
+            
+            let livrosObtidos = fetch(urlAPI)
+                .then((response) => response.json())
+                .then(data => livrosObtidos = data);
+            return livrosObtidos;
+        },
+        //BuscarLivroPorId
+        BuscarLivroPorId: function (idLivroBuscado) {
+            let livroBuscado = fetch(`${urlAPI}${idLivroBuscado}`)
+                .then((response) => response.json())
+                .then(data => livroBuscado = data)
+            return livroBuscado;
+        },
+        //SalvarLivro
+        SalvarLivro: async function (livroASerSalvo) {
+            const metodoSalvar = 'POST';
+            let livroModelo = livroASerSalvo;
+            var livroRetorno;
+            await fetch(urlAPI, {
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    method: metodoSalvar,
+                    body: JSON.stringify({
+                        autor: livroModelo.autor,
+                        titulo: livroModelo.titulo,
+                        editora: livroModelo.editora,
+                        lancamento: livroModelo.lancamento,
+                    })
+                })
+                .then((response) => response.json())
+                .then(data => livroRetorno = data)
+            return livroRetorno;
+        },
+
+        AtualizarLivro: async function (livroASerAtualizado) {
+            const metodoEditar = 'PUT';
+            let livroModelo = livroASerAtualizado;
+            var livro;
+            var retorno;
+            await fetch(`${urlAPI}${livroModelo.id}`, {
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    method: metodoEditar,
+                    body: JSON.stringify({
+                        id: livroModelo.id,
+                        autor: livroModelo.autor,
+                        titulo: livroModelo.titulo,
+                        editora: livroModelo.editora,
+                        lancamento: livroModelo.lancamento,
+                    })
+                })
+                .then((response) => retorno = response.status)
+            return retorno;
+        },
+
+        ExcluirLivro: async function (livroASerExcluido) {
+            const metodoExcluir = 'DELETE';
+            let livroModelo = livroASerExcluido.getData();
+            let idASerDeletado = livroModelo.id;
+            await fetch(`${urlAPI}${idASerDeletado}`, {
+                method: metodoExcluir
+            })
+        }
+    });
+});
